87e824bbbdb4d3d471113819f99896bffec5ae52
aravindh <anathan@zetaglobal.com>
2019-09-23 01:02:17 +0530

    update budget bucket values on budget income

--- app/models/budget_bucket.rb
+++ app/models/budget_bucket.rb
@@ -5,4 +5,8 @@ class BudgetBucket < ActiveRecord::Base
 	has_many :budget_incomes
 	has_many :budget_expense_transactions
 	belongs_to :budget
+
+	def add_income(amount)
+		self.amount += amount
+	end
 end
